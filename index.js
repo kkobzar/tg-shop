8c67fad862e28ebdba25d7406c4c95a9c760c3c9
Kiril Kobzar <kirilkobzar@gmail.com>
2022-12-17 18:34:31 +0200

    bot init

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Hello'));
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
